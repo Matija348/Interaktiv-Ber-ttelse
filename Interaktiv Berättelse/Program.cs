58c639740126ca186526b3906ded2896365ddac6
Matija Zezelj <matija.zezelj@elev.ga.ntig.se>
2024-09-02 09:19:20 +0200

    Vall

--- Interaktiv Berättelse/Program.cs
+++ Interaktiv Berättelse/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+class InteraktivBerattelse
+{
+    static void Main()
+    {
+        // Introduktion
+        Console.WriteLine("Du befinner dig i en mystisk skog och stöter på en korsning.");
+        Console.WriteLine("Vill du gå till vänster eller höger? (vänster/höger)");
+
+        // Användarens första beslut
+        string val1 = Console.ReadLine().ToLower();
